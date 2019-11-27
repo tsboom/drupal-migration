cf51c4322a98eac154972d10497876d9d7cb8efe
Tiffany Schoneboom <thschone@umd.edu>
2019-11-27 12:33:14 -0500

    removed attachment table and fixed relative links in importer_utils.py

--- importer_utils.py
+++ importer_utils.py
@@ -8,13 +8,27 @@ def remove_attachment_table(page_html):
     
     replace_string = "<p>This page has attachments. They can be viewed by clicking the \"...\" to the right of the title of this page. See <a href=\"https://usmai.org/confluence/display/MAIN/Working+with+attachments\">this documentation on working with attachments</a> for further explanation of using attachments in Confluence</p>"
     
-    clean_soup = page_html
-    
     if attachment_table:
-        # attachment_table.decompose()
-        clean_soup = attachment_table.replace_with(replace_string)
-        # clean_soup = soup.html.append(BeautifulSoup(replace_string, features="html.parser"))
+        # remove attachment table
+        attachment_table.decompose()
         
-        # clean_soup = BeautifulSoup(str(soup).replace(attachment_table.text, replace_string), features="html.parser")
+        # add replace_string
+        soup.find('div', {"class": "content"}).append(BeautifulSoup(replace_string, 'html.parser'))
+        
+        clean_soup = str(soup)
+    else: 
+        clean_soup = page_html
     
     return clean_soup
+    
+def fix_relative_links(page_html):
+    soup = BeautifulSoup(page_html, features="html.parser")
+    
+    for link in soup.find_all('a', href=True):
+        href = link.get('href')
+        if href.startswith('/'):
+            prefix = 'http://usmai.umd.edu'
+            link['href'] = prefix + href
+            
+    fixed_soup = str(soup)
+    return fixed_soup
