5cb95d04a0421c567a940a09cf23bbf9665b629c
Tiffany Schoneboom <thschone@umd.edu>
2019-12-05 19:24:23 -0500

    add internal and external link csv writing

--- importer_utils.py
+++ importer_utils.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 def remove_attachment_table(soup):
     attachment_table = soup.find(id="attachments")
     
-    replace_string = "<p>This page has attachments. They can be viewed by clicking the \"...\" to the right of the title of this page. See <a href=\"https://usmai.org/confluence/display/MAIN/Working+with+attachments\">this documentation on working with attachments</a> for further explanation of using attachments in Confluence</p>"
+    replace_string = "<p>This page has attachments. They can be viewed by clicking the \"...\" to the right of the title of this page. See <a href=\"https://usmai.org/portal/display/MAIN/Working+with+attachments\">this documentation on working with attachments</a> for further explanation of using attachments in Confluence</p>"
     
     if attachment_table:
         # remove attachment table
@@ -15,7 +15,7 @@ def remove_attachment_table(soup):
         
         clean_soup = str(soup)
     else: 
-        clean_soup = soup
+        clean_soup = str(soup)
     
     return clean_soup
     
@@ -29,19 +29,27 @@ def fix_relative_links(soup):
     fixed_soup = str(soup)
     return fixed_soup
     
-def write_links(ref_id, soup, f):
-    link_list = []
+def write_links(ref_id, soup, ext_writer, int_writer):
+    external_links = []
+    external_links.append(ref_id)
     usmai_links = []
     usmai_links.append(ref_id)
     for link in soup.find_all('a', href=True):
-        link_text = link['href']
-        if "usmai.umd.edu" in link_text:
-            usmai_links.append(link_text)
-        else:
-            link_list.append(link['href'])
-    total_list = usmai_links + link_list
-    write_line = ','.join([i for i in total_list]) + '\n'
-    f.write(write_line)
+        href = link.get('href')
+        # skip anchor tags and emails
+        if '#' in href or 'mailto:' in href:
+            continue
+        else: 
+            # save usmai links
+            if "usmai.umd.edu" in href:
+                usmai_links.append(href)
+            else:
+                # save external links
+                external_links.append(href)
+
+    # write links to csvs
+    int_writer.writerow(usmai_links)
+    ext_writer.writerow(external_links)
     
     
         
